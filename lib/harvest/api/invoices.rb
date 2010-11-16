01bcff4c64bd105c506cc2dff25f9ab9ad5b0326
Mark Van Holstyn <mvanholstyn@gmail.com>
2010-11-16 18:30:51 -0500

    remove left over debugging puts

--- lib/harvest/api/invoices.rb
+++ lib/harvest/api/invoices.rb
@@ -8,7 +8,6 @@ module Harvest
       # TODO: Document
       def line_items(invoice)
         invoice_with_line_items = find(invoice.id)
-        puts invoice_with_line_items.csv_line_items
         line_items = FasterCSV.parse(invoice_with_line_items.csv_line_items, :headers => true)
         line_items.map do |line_item|
           Harvest::LineItem.new(line_item.to_hash)
