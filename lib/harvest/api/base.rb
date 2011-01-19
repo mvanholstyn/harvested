0565827b48bb1e7d8f9532ce509095b4f5a639c5
Matthew Seeley <matthew@threadlight.com>
2011-01-19 15:22:40 -0500

    Fixing to_string silliness.

--- lib/harvest/api/base.rb
+++ lib/harvest/api/base.rb
@@ -22,7 +22,7 @@ module Harvest
           puts '---[harvest] ' + path
           response = HTTParty.send(method, "#{credentials.host}#{path}", :query => options[:query], :body => options[:body], :headers => {"Accept" => "application/xml", "Content-Type" => "application/xml; charset=utf-8", "Authorization" => "Basic #{credentials.basic_auth}", "User-Agent" => "Harvestable/#{Harvest::VERSION}"}.update(options[:headers] || {}), :format => :plain)
 
-          puts '---[harvest] -- ' + response.code
+          puts '---[harvest] -- ' + response.code.to_s
           response
 
           case response.code
